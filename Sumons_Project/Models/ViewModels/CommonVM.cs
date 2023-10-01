8eb1469264475765b32a3ab957965599f089cea2
islamfa1 <faridul.islam@bangjin.com>
2023-10-01 18:14:37 +0600

    model

--- Sumons_Project/Models/ViewModels/CommonVM.cs
+++ Sumons_Project/Models/ViewModels/CommonVM.cs
@@ -10,6 +10,5 @@ namespace LabTestRegister.Models.ViewModels
         public string Id { get; set; }
         public string StatusCode { get; set; }
         public string StatusMessage { get; set; }
-        public string StatusMessage1 { get; set; }
     }
 }
